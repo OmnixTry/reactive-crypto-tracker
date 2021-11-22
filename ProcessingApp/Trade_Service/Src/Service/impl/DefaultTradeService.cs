f0b3b3156946d7f9fb493ed5a682d1168287b8b8
OmnixTry <Omnix.try@gmail.com>
2021-11-22 22:19:52 +0200

    add resilient storage and connection string

--- ProcessingApp/Trade_Service/Src/Service/impl/DefaultTradeService.cs
+++ ProcessingApp/Trade_Service/Src/Service/impl/DefaultTradeService.cs
@@ -50,21 +50,56 @@ namespace ProcessingApp.Trade_Service.Src.Service.impl
             IObservable<Dictionary<string, object>> input)
         {
             // TODO: Add implementation to produce trading events
-            return Observable.Never<MessageDTO<MessageTrade>>();
+            return input
+                .Where(message => MessageMapper.IsTradeMessageType(message))
+                .Select(message => MessageMapper.MapToTradeMessage(message));
         }
 
         private IObservable<Trade> MapToDomainTrade(IObservable<MessageDTO<MessageTrade>> input)
         {
             // TODO: Add implementation to mapping to com.example.part_10.domain.Trade
-            return Observable.Never<Trade>();
+            return input.Select(message => DomainMapper.MapToDomain(message));
         }
 
-        private static IObservable<int> ResilientlyStoreByBatchesToAllRepositories(
-            IObservable<Trade> input,
-            ITradeRepository tradeRepository1,
-            ITradeRepository tradeRepository2)
+        private IObservable<int> ResilientlyStoreByBatchesToAllRepositories(
+                IObservable<Trade> input,
+                ITradeRepository tradeRepository1,
+                ITradeRepository tradeRepository2)
         {
-            return Observable.Never<int>();
+            // behaviour subject unlike rerplay subject stores only one value
+            // in always needs a default value
+            var subject = new BehaviorSubject<object>(new object());
+            subject.OnNext(new object()); // publishing value to subject
+
+            IObservable<(long, object)> bufferBoundaries = Observable
+                .Interval(TimeSpan.FromSeconds(1)) // generates values every specified period of time
+                .Zip(subject, (i, o) => (i, o));
+            // combines elements with the same index from 2 observables together
+            // if one observable doesn't come for long the other one's elements wait
+
+            // this way we can controll when to finish buffering
+            // if we emit value in subject, new time range starts
+
+            return input.Buffer(bufferBoundaries) // bufferizes observable values into list
+                .SelectMany(trades => // works for all the gathered lists
+                {
+                    List<Trade> tradesList = trades.ToList();
+
+                    return SaveSafely(tradeRepository1, tradesList) // saves to one db then saves to the other
+                        .Merge(SaveSafely(tradeRepository2, tradesList))
+                        .Do(onNext: i => { }, onCompleted: () => subject.OnNext(new object()));
+                    // when both saves were successfull, emits new value to subject, starting new buffer
+
+                });
+        }
+
+        private IObservable<int> SaveSafely(ITradeRepository tradeRepository, List<Trade> tradesList)
+        {
+            // if no result received during one second (timeout function) send error messages
+            // Retry catches the error mesages, and tries again
+            return tradeRepository
+                .SaveAll(tradesList)
+                .Timeout(TimeSpan.FromSeconds(1)).Retry(100);
         }
     }
 }
